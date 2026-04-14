10eadd38af814bf6eda302ba4f17eb902457a862
Juana Correa Luna <juanicluna@gmail.com>
2026-04-14 20:14:10 -0300

    feat: creacion de teacher y sus atributos

---
+++ src/main/kotlin/ar/edu/unsam/pds/models/user/Teacher.kt
@@ -0,0 +1,25 @@
+package ar.edu.unsam.pds.models.user
+
+import ar.edu.unsam.pds.models.Classroom
+import ar.edu.unsam.pds.models.Course
+import ar.edu.unsam.pds.models.Event
+import ar.edu.unsam.pds.models.Schedule
+
+class Teacher(name: String,
+                    lastName: String,
+                    email: String, ) : User(name, lastName, email) {
+
+    val courses: MutableSet<Course> = mutableSetOf()
+
+    fun bookClassroom(schedule: Schedule, classroom: Classroom, event: Event, course: Course){
+        event.addSchedule(schedule)
+        schedule.event = event
+        if (validateCourse(course)) {
+            event.attachCourse(course)
+        }
+        schedule.classroom = classroom
+    }
+
+    private fun validateCourse(course: Course) = courses.contains(course)
+
+}
