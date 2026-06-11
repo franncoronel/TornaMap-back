436cb129aa517ea959e53e4e768408f653403dc7
Melody Oviedo Morales <moviedomorales@estudiantes.unsam.edu.ar>
2026-06-11 11:54:16 -0300

    feat: implemento mapper para eventos institucionales

---
+++ src/main/kotlin/ar/edu/unsam/pds/mappers/InstitutionalEventMapper.kt
@@ -0,0 +1,29 @@
+package ar.edu.unsam.pds.mappers
+
+import ar.edu.unsam.pds.dto.response.InstitutionalEventResponseDto
+import ar.edu.unsam.pds.models.Event
+
+object InstitutionalEventMapper {
+
+    fun buildDto(event: Event): InstitutionalEventResponseDto {
+        val schedule = event.schedules.firstOrNull()
+
+        return InstitutionalEventResponseDto(
+            id = event.id.toString(),
+            name = event.name,
+            type = event.type.name,
+            details = event.details,
+            startDate = schedule?.date,
+            startTime = schedule?.startTime?.toString(),
+            endTime = schedule?.endTime?.toString(),
+            location = if (schedule?.isVirtual == true)
+                "Virtual"
+            else
+                schedule?.classroom?.let {
+                    "${it.name} - ${it.building?.name ?: ""}"
+                },
+            isVirtual = schedule?.isVirtual ?: false,
+            professors = schedule?.assignedUsers!!.map { it.lastName+", "+it.name }
+        )
+    }
+}
